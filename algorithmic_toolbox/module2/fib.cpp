b3bf49e46f19240b65281152b01209a49960a124
Alexey <alexey.tkachenko@laplace-research.com>
2024-07-16 06:30:20 +0800

    update

--- algorithmic_toolbox/module2/fib.cpp
+++ algorithmic_toolbox/module2/fib.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 
-int fib(int n)
-{
-    if (n <= 1)
-        return n;
-    int prev[] = { 0, 1 };
+#define TEST 0
 
-    for(int i = 2; i <= n; ++i)
-    {
-        const int newFib = prev[0] + prev[1];
-        prev[0] = prev[1];
-        prev[1] = newFib;
-    }
-    return prev[1];
+int fib(int n) {
+  if (n <= 1)
+    return n;
+  int prev[] = {0, 1};
+#if TEST
+  std::cout << prev[0] << '\n' << prev[1] << '\n';
+#endif // TEST
+
+  for (int i = 2; i <= n; ++i) {
+    const int newFib = prev[0] + prev[1];
+    prev[0] = prev[1];
+    prev[1] = newFib;
+#if TEST
+    std::cout << newFib << '\n';
+#endif // TEST
+  }
+#if TEST
+  std::cout << '\n';
+#endif // TEST
+  return prev[1];
 }
 
 int main() {
-    int n = 0;
-    std::cin >> n;
+#if TEST
+  int n = 10;
+#else
+  int n = 0;
+  std::cin >> n;
+#endif // TEST
 
-    std::cout << fib(n) << '\n';
-    return 0;
+  std::cout << fib(n) << '\n';
+  return 0;
 }
--- algorithmic_toolbox/module2/lcm.cpp
+++ algorithmic_toolbox/module2/lcm.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int gcd(int a, int b) {
-    if(b == 0)
-        return a;
-    return gcd(b, a % b);
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
 }
 
 long long lcm(int a, int b) {
-    const long long gcd = ::gcd(a,b);
-    const long long a_over_gcd = a / gcd;
-    return a_over_gcd * b;
+  const long long gcd = ::gcd(a, b);
+  const long long a_over_gcd = a / gcd;
+  return a_over_gcd * b;
 }
 
 int main() {
